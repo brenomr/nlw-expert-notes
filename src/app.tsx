3c2006957869fee3ee596a0fd602f677507a69cd
Breno <brenocartoon@hotmail.com>
2024-02-09 17:44:22 -0300

    feat: implement note card delete function

--- src/app.tsx
+++ src/app.tsx
@@ -35,6 +35,14 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray));
   }
 
+  const onNoteDeleted = (id: string) => {
+    const notesArray = notes.filter(note => note.id !== id);
+
+    setNotes(notesArray);
+
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+  }
+
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value;
 
@@ -62,7 +70,7 @@ export function App() {
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {
-          filteredNotes.map(note => (<NoteCard note={note} key={note.id} />))
+          filteredNotes.map(note => (<NoteCard note={note} key={note.id} onNoteDeleted={onNoteDeleted} />))
         }
       </div>
     </div>
