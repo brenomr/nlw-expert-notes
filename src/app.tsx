5455bcfd2c2248d222e7e8470d032319750da7ce
Breno <brenocartoon@hotmail.com>
2024-02-08 23:14:25 -0300

    feat: add first step to allow note input and save

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,26 @@
+import logo from './assets/logo-nlw-expert.svg';
+import { NewNoteCard } from './components/new-note-card';
+import { NoteCard } from './components/note-card';
+
 export function App() {
   return (
-    <>
-    <h1>Starting project!</h1>
-    </>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt="Logo do NLW Expert" />
+      <form className='w-full' action="">
+        <input
+          type='text'
+          placeholder='Busque suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700'/>
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <NewNoteCard />
+        <NoteCard note={{date: new Date('2024-01-02'), content: 'First card content!'}} />
+        <NoteCard note={{date: new Date('2024-01-05'), content: 'Second card content!'}} />
+      </div>
+    </div>
   )
 }
